3556e272adce939f0d905f52fb182a1c400f314f
jtarver1 <jtarver1@jsu.stu.edu>
2023-09-01 11:46:32 -0500

    Added methods

--- Class2.java
+++ Class2.java
@@ -1,12 +1,17 @@
 /**
  *
- * @author Your Name
+ * @author jtarver1
  */
 
-public class Class2 {
-
+public class Class2{
     private void f1() {
         // v1.0
     }
-
+    private void f2(){
+        //v2.0
+    }
+    private void f3(){
+        //v3.0
+    }
 }
+
