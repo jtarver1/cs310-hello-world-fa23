9fd99c604dd042b78a21df0341d0c3e59605e183
jtarver1 <143665492+jtarver1@users.noreply.github.com>
2023-09-01 21:45:24 -0500

    Update Class1.java f3

--- Class1.java
+++ Class1.java
@@ -12,10 +12,7 @@ public class Class1 {
         //v2.0
     }
     private void f3(){
-        //v3.0
-    }
-    private void f4(){
-        //v3.0 under development
+        //v3.0 revised for hotfix
     }
 
 }
